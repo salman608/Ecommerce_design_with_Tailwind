2bbf04358011a9d3a9e9df7efa66c202cb6191e4
salman608 <salman608@gmail.com>
2022-11-18 02:31:07 +0600

    color screen customized

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px',
+    },
+    container:{
+      center:'true',
+      padding:'1rem',
+    },
+    colors:{
+      primary:'#FD3D57',
+    },
     extend: {
       fontfamily:{
         poppins: "'Poppins', sans-serif",
