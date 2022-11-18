0ea6f5c24e6e63bbf5c844f6c43210eca1fd1108
salman608 <salman608@gmail.com>
2022-11-18 17:19:45 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens:{
-      sm:'576px',
-      md:'768px',
-      lg:'992px',
-      xl:'1200px',
-    },
-    containers:{
-      center:'true',
-      padding:'1rem',
-    },
-    colors:{
-      primary:'#FD3D57',
-    },
     extend: {
       fontfamily:{
         poppins: "'Poppins', sans-serif",
         roboto: "'Roboto', sans-serif",
-      } 
+      } ,
+      screens:{
+        sm:'576px',
+        md:'768px',
+        lg:'992px',
+        xl:'1200px',
+      },
+      container:{
+        center:true,
+        padding:'2rem',
+      },
+      colors:{
+        primary:'#FD3D57',
+      },
 
     },
   },
